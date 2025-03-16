b71d1cf5232e1e65249ac66806a9a450566c8e89
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-03-16 08:55:07 -0300

    feat: implement RabbitMQ message sending and receiving functionality

--- dotnet/Receive/Receive.cs
+++ dotnet/Receive/Receive.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("hello", false, false, false, null);
+
+Console.WriteLine("[*] Waiting for messages...");
+await Task.Delay(TimeSpan.FromSeconds(1));
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) => {
+  var body = ea.Body.ToArray();
+  var message = Encoding.UTF8.GetString(body);
+  Console.WriteLine($"[x] Received {message}");
+  Task.Delay(TimeSpan.FromSeconds(0.5));
+  return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync("hello", autoAck: true,consumer: consumer);
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
--- dotnet/Send/Send.cs
+++ dotnet/Send/Send.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+int counter = 1;
+
+for(int i = 0; i < 1000000; i++){
+  string message = $"Hello World! {counter}";
+  var body = Encoding.UTF8.GetBytes(message);
+  
+  await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+  Console.WriteLine($"[x] Sent {message}");
+  counter++;
+}
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
